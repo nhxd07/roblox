0cdd1ae1ff0b1ef986a0733e88fb4e223daba9b4
Nhu Hai <103500732+nhxd07@users.noreply.github.com>
2023-12-29 09:03:23 +0700

    Add files via upload

--- bu.lua
+++ bu.lua
@@ -92,43 +92,26 @@ game:GetService("UserInputService").InputChanged:Connect(
 )
 end
 
-local ThunderScreen = Instance.new("ScreenGui")
-local ThunderToggleUI = Instance.new("TextButton")
-local ThunderCornerUI = Instance.new("UICorner")
-local ThunderImageUI = Instance.new("ImageLabel")
-local ThunderImageUI = Instance.new("ImageLabel")
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+        
+ScreenGui.Name = "ImageButton"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-        ThunderScreen.Name = "ThunderScreen"
-        ThunderScreen.Parent = game.CoreGui
-        ThunderScreen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-        ThunderToggleUI.Name = "ThunderToggleUI"
-        ThunderToggleUI.Parent = ThunderScreen
-        ThunderToggleUI.BackgroundColor3 = Color3.fromRGB(31,31,31)
-        ThunderToggleUI.BorderSizePixel = 0
-        ThunderToggleUI.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-        ThunderToggleUI.Size = UDim2.new(0, 50, 0, 50)
-        ThunderToggleUI.Font = Enum.Font.SourceSans
-        ThunderToggleUI.Text = ""
-        ThunderToggleUI.TextColor3 = Color3.fromRGB(0, 0, 0)
-        ThunderToggleUI.TextSize = 14.000
-        ThunderToggleUI.Draggable = true
-        ThunderToggleUI.MouseButton1Click:Connect(function()
-        game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
-        game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
-        end)
-
-        ThunderCornerUI.Name = "ThunderCornerUI"
-        ThunderCornerUI.Parent = ThunderToggleUI
-
-        ThunderImageUI.Name = "MODILEMAGE"
-        ThunderImageUI.Parent = ThunderToggleUI
-        ThunderImageUI.BackgroundColor3 = Color3.fromRGB(192,192,192)
-        ThunderImageUI.BackgroundTransparency = 1.000
-        ThunderImageUI.BorderSizePixel = 0
-        ThunderImageUI.Position = UDim2.new(0.0, 0, 0.0, 0)
-        ThunderImageUI.Size = UDim2.new(0, 50, 0, 50)
-        ThunderImageUI.Image = "http://www.roblox.com/asset/?id=14288624550"
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
+ImageButton.Size = UDim2.new(0, 50, 0, 50)
+ImageButton.Draggable = true
+ImageButton.Image = "http://www.roblox.com/asset/?id=12781257228"
+ImageButton.MouseButton1Down:connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
+    game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
+end)
+UICorner.Parent = ImageButton
 
 _G.Logo = 14103696428
 _G.Logo1 = 14288624550
